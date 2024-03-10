afc1731bcde803a0018b2086cfad9d9e70154938
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-03-10 20:59:54 -0300

    feat: ensure Login will not load spinner and error on start

---
+++ src/presentation/contexts/form/form-context.ts
@@ -0,0 +1,11 @@
+import { createContext } from 'react'
+
+export type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
+
+export default createContext<StateProps>({
+  isLoading: false,
+  errorMessage: ''
+})
