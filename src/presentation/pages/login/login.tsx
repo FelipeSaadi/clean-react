df15743e078bf9dd6cb43c09e92e9e269d51493d
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-03-10 16:47:56 -0300

    refactor: imports and exports

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import LoginHeader from '@/presentation/components/login-header/login-header'
-import Spinner from '@/presentation/components/spinner/spinner'
-import Footer from '@/presentation/components/footer/footer'
-import Input from '@/presentation/components/input/input'
+import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 
 const Login: React.FC = () => {
   return (
@@ -15,10 +12,7 @@ const Login: React.FC = () => {
         <Input type="password" name="password" placeholder='Digite sua senha' />
         <button className={Styles.submit} type='button'>Entrar</button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner} />
-          <span className={Styles.error}>Erro</span>
-        </div>
+        <FormStatus />
       </form>
       <Footer />
     </div>
