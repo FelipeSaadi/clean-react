5fceb57bb758f1f46599850eaf7932bd4ae72cc2
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-03-13 23:19:03 -0300

    feat: ensure Login present valid state if password is valid

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -9,10 +9,10 @@ const Input: React.FC<Props> = (props: Props) => {
   const { state, setState } = useContext(Context)
   const error = state[`${props.name}Error`]
   const getStatus = (): string => {
-    return '🔴'
+    return error ? '🔴' : '🟢'
   }
   const getTitle = (): string => {
-    return error
+    return error || 'ok'
   }
   const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
     setState({
