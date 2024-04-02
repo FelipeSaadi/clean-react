409ca41608c20a52d5eef84dade91291e20243e0
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-04-01 22:27:04 -0300

    refactor: update eslint-standard-with-typescript version

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -1,32 +1,32 @@
 import { EmailValidation, MinLengthValidation, RequiredFieldValidation, ValidationBuilder as sut } from '@/validation/validators'
-import faker from 'faker'
+import { faker } from '@faker-js/faker'
 
 describe('ValidationBuilder', () => {
   test('Should return RequiredFieldValidation', () => {
-    const field = faker.database.column()
+    const field: string = faker.database.column()
 
     const validations = sut.field(field).required().build()
     expect(validations).toEqual([new RequiredFieldValidation(field)])
   })
 
   test('Should return EmailValidation', () => {
-    const field = faker.database.column()
+    const field: string = faker.database.column()
 
     const validations = sut.field(field).email().build()
     expect(validations).toEqual([new EmailValidation(field)])
   })
 
   test('Should return MinLengthValidation', () => {
-    const field = faker.database.column()
-    const length = faker.datatype.number()
+    const field: string = faker.database.column()
+    const length: number = faker.number.int(3)
 
     const validations = sut.field(field).min(length).build()
     expect(validations).toEqual([new MinLengthValidation(field, length)])
   })
 
   test('Should return a list of validations', () => {
-    const field = faker.database.column()
-    const length = faker.datatype.number()
+    const field: string = faker.database.column()
+    const length: number = faker.number.int(3)
 
     const validations = sut.field(field).required().min(length).email().build()
     expect(validations).toEqual([
