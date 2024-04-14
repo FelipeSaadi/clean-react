c817ea62d15e99489a9f545d752d32be89954d58
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-04-13 23:54:27 -0300

    feat: ensure Signup calls SaveAccessToken on success

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Styles from './signup-styles.scss'
-import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
+import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
 import { type Validation } from '@/presentation/protocols/validation'
-import { type AddAccount } from '@/domain/usecases'
+import { type SaveAccessToken, type AddAccount } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
   addAccount: AddAccount
+  saveAccessToken: SaveAccessToken
 }
 
-const Signup: React.FC<Props> = ({ validation, addAccount }: Props) => {
+const Signup: React.FC<Props> = ({ validation, addAccount, saveAccessToken }: Props) => {
+  const navigate = useNavigate()
   const [state, setState] = useState({
     isLoading: false,
     name: '',
@@ -42,12 +45,14 @@ const Signup: React.FC<Props> = ({ validation, addAccount }: Props) => {
           return
         }
         setState({ ...state, isLoading: true })
-        await addAccount.add({
+        const account = await addAccount.add({
           name: state.name,
           email: state.email,
           password: state.password,
           passwordConfirmation: state.passwordConfirmation
         })
+        await saveAccessToken.save(account.accessToken)
+        navigate('/', { replace: true })
       } catch (error) {
         setState({
           ...state,
