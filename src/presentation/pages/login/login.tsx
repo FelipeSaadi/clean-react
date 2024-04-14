5db73b8a33b4ccba80eb9650fa9e7df1c3d945a1
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2024-04-14 13:45:22 -0300

    refactor: add SubmitButton

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Styles from './login-styles.scss'
-import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
+import { LoginHeader, Footer, Input, FormStatus, SubmitButton } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { type Validation } from '@/presentation/protocols/validation'
 import { type Authentication, type SaveAccessToken } from '@/domain/usecases'
@@ -16,6 +16,7 @@ const Login: React.FC<Props> = ({ validation, authentication, saveAccessToken }:
   const navigate = useNavigate()
   const [state, setState] = useState({
     isLoading: false,
+    isFormInvalid: true,
     email: '',
     password: '',
     emailError: '',
@@ -24,10 +25,14 @@ const Login: React.FC<Props> = ({ validation, authentication, saveAccessToken }:
   })
 
   useEffect(() => {
+    const emailError = validation.validate('email', state.email)
+    const passwordError = validation.validate('password', state.password)
+
     setState({
       ...state,
-      emailError: validation.validate('email', state.email),
-      passwordError: validation.validate('password', state.password)
+      emailError,
+      passwordError,
+      isFormInvalid: !!emailError || !!passwordError
     })
   }, [state.email, state.password])
 
@@ -35,7 +40,7 @@ const Login: React.FC<Props> = ({ validation, authentication, saveAccessToken }:
     void (async (): Promise<void> => {
       event.preventDefault()
       try {
-        if (state.isLoading || state.emailError || state.passwordError) {
+        if (state.isLoading || state.isFormInvalid) {
           return
         }
         setState({ ...state, isLoading: true })
@@ -63,7 +68,7 @@ const Login: React.FC<Props> = ({ validation, authentication, saveAccessToken }:
           <h2>Login</h2>
           <Input type="email" name="email" placeholder='Digite seu e-mail' />
           <Input type="password" name="password" placeholder='Digite sua senha' />
-          <button data-testid="submit" disabled={!!state.emailError || !!state.passwordError} className={Styles.submit} type='submit'>Entrar</button>
+          <SubmitButton text='Entrar' />
           <Link data-testid="signup-link" to="/signup" className={Styles.link}>Criar conta</Link>
           <FormStatus />
         </form>
